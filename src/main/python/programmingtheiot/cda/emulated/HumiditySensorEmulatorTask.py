4e288401b47311987c8353e4e26257bd0e85c908
Eva <vaee2004@gmail.com>
2025-03-01 13:01:09 +0100

    cambios

--- src/main/python/programmingtheiot/cda/emulated/HumiditySensorEmulatorTask.py
+++ src/main/python/programmingtheiot/cda/emulated/HumiditySensorEmulatorTask.py
@@ -23,19 +23,18 @@ class HumiditySensorEmulatorTask(BaseSensorSimTask):
 	"""
 
 	def __init__(self, dataSet = None):
-		super(HumiditySensorEmulatorTask, self).__init__(name=ConfigConst.HUMIDITY_SENSOR_NAME,
-														 typeID=ConfigConst.HUMIDITY_SENSOR_TYPE)
+		super(HumiditySensorEmulatorTask, self).__init__(name=ConfigConst.HUMIDITY_SENSOR_NAME, typeID=ConfigConst.HUMIDITY_SENSOR_TYPE)
 
 		enableEmulation = ConfigUtil().getBoolean(ConfigConst.CONSTRAINED_DEVICE, ConfigConst.ENABLE_EMULATOR_KEY)
 
 		self.sh = SenseHAT(emulate=enableEmulation)
 
 
-def generateTelemetry(self) -> SensorData:
-		sensorData = SensorData(name=self.getName(), typeID=self.getTypeID())
-		sensorVal = self.sh.environ.humidity
+	def generateTelemetry(self) -> SensorData:
+			sensorData = SensorData(name=self.getName(), typeID=self.getTypeID())
+			sensorVal = self.sh.environ.humidity
 
-		sensorData.setValue(sensorVal)
-		self.latestSensorData = sensorData
+			sensorData.setValue(sensorVal)
+			self.latestSensorData = sensorData
 
-		return sensorData
+			return sensorData
